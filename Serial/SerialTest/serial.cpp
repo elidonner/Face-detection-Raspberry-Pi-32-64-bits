0e929d7ea5b68b141f90d0bd7a55e2f30fff63bc
ethandonlon <94150522+ethandonlon@users.noreply.github.com>
2022-07-07 16:21:04 -0400

    edited serial and sending to pi

--- Serial/SerialTest/serial.cpp
+++ Serial/SerialTest/serial.cpp
@@ -8,9 +8,59 @@
 #include <signal.h>
 #include <pigpio.h>
 using namespace std;
+
+const uint8_t numBytes = 32;
+uint8_t receivedBytes[numBytes];
+uint8_t numReceived = 0;
+
+
+int recvBytesWithStartEndMarkers() {
+    static bool recvInProgress = false;
+    static uint8_t ndx = 0;
+    uint8_t startMarker = 0x3C; //this is the start marker <
+    uint8_t endMarker = 0x3E; //this is the start marker >
+    uint8_t rb;
+   
+    while (serDataAvailable () > 0 && newData == false) {
+        rb = serReadByte(serial_port);
+
+        if (recvInProgress == true) {
+            if (rb != endMarker) {
+                receivedBytes[ndx] = rb;
+                ndx++;
+                if (ndx >= numBytes) {
+                    ndx = numBytes - 1;
+                }
+            }
+            else {
+                receivedBytes[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                numReceived = ndx;  // save the number for use when printing
+                ndx = 0;
+                showNewData();
+                break;
+            }
+        }
+
+        else if (rb == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+void showNewData() {
+    Serial.print("This just in (HEX values)... ");
+    for (uint8_t n = 0; n < numReceived; n++) {
+      printf("%c", receivedBytes[n]) ;
+    }
+    Serial.println();
+}
+
+
 int main ()
 {
-  int serial_port ;
+
+  int serial_port, writeFlag;
   char dat;
   char serialName[] = "/dev/ttyS0";
 
@@ -25,25 +75,19 @@ int main ()
     return 1;
   }
 
-  //if(serWriteByte(serial_port, 1))
-  //{
-    //cout<<"Failed write";
-  //}
-
   char msg[] = "<hello world>";
-  if(serWrite(serial_port, msg, 13))
-  {
-    cout<<"Failed write";
-  }
 
   while(1)
   {
-
     if(serDataAvailable (serial_port) )
-	{
-		dat = serReadByte(serial_port);		/* receive character serially*/
-		printf("%c", dat) ;
+	  {
+      writeFlag = recvBytesWithStartEndMarkers();
+      dat = serReadByte(serial_port);		/* receive character serially*/
     }
+    if 
+    (
+
+    )
   }
 
 }
